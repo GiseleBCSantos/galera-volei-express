7303418277bf66c318a2ce1eebdaa65d32f69501
GiseleBCSantos <giselebianca57@gmail.com>
2025-10-08 08:40:07 -0300

    Add arena router for handling arena-related API routes

---
+++ src/presentation/routes/rotas_arenas.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { ArenasController } from "../controllers/arenas.controller";
+
+export const arena_router = Router();
+const arena_controller = new ArenasController();
+
+arena_router.get("/", arena_controller.getAll);
+arena_router.get("/two", arena_controller.getAll2);
+arena_router.get("/three", arena_controller.getAll3);
+arena_router.get("/:id", arena_controller.findById);
+arena_router.post("/", arena_controller.createOne);
