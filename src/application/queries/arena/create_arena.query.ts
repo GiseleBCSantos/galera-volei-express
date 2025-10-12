77c08799c7d21e59902015d84338d000e575f2e0
GiseleBCSantos <giselebianca57@gmail.com>
2025-10-12 17:08:40 -0300

    feat: Add validation for arena creation to ensure required fields are provided

--- src/application/queries/arena/create_arena.query.ts
+++ src/application/queries/arena/create_arena.query.ts
@@ -1,9 +1,18 @@
 import { v4 as uuid } from "uuid";
 import { arenas } from "../../repositories/arena_repository";
+import { InvalidValuesException } from "../../../presentation/exceptions/InvalidValuesException";
 
 export class CreateArena {
   constructor() {}
-  public execute(nome: string, zona: string, endereco?: string, geolocalizacao?: string) {
+  public execute(
+    nome: string,
+    zona: string,
+    endereco?: string,
+    geolocalizacao?: string
+  ) {
+    if (!nome || !zona) {
+      throw new InvalidValuesException("Dados inválidos");
+    }
     const newArena = { id: uuid(), nome, zona, endereco, geolocalizacao };
     arenas.push(newArena);
     return newArena;
