39ad4f2ba2fb053b011a4a0c2788ee1ad5e6ed35
GiseleBCSantos <giselebianca57@gmail.com>
2025-10-12 13:19:49 -0300

    feat: add InvalidValuesException class for error handling

---
+++ src/presentation/exceptions/InvalidValuesException.ts
@@ -0,0 +1,6 @@
+export class InvalidValuesException extends Error {
+  constructor(message: string, public statusCode: number = 400) {
+    super(message);
+    this.name = "InvalidValuesException";
+  }
+}
