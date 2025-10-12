abc64a49bf6dc62dc90a11874ff0ab59adc032e8
GiseleBCSantos <giselebianca57@gmail.com>
2025-10-12 17:09:05 -0300

    feat: Add tipo property to CreatePartidaQuery for partida type specification

--- src/application/queries/partida/create_partida.query.ts
+++ src/application/queries/partida/create_partida.query.ts
@@ -1,7 +1,7 @@
 import { v4 as uuid } from "uuid";
 import { partidas } from "../../repositories/partida_repository";
 import { InvalidValuesException } from "../../../presentation/exceptions/InvalidValuesException";
-import { STATUS_PARTIDA } from "../../models/partida_model";
+import { STATUS_PARTIDA, TIPO_PARTIDA } from "../../models/partida_model";
 
 export class CreatePartidaQuery {
   constructor() {}
@@ -11,7 +11,8 @@ export class CreatePartidaQuery {
     data_partida: Date,
     arenaId: string,
     jogadoresIds: string[],
-    num_max_jogadores?: number
+    num_max_jogadores?: number,
+    tipo: (typeof TIPO_PARTIDA)[keyof typeof TIPO_PARTIDA] = TIPO_PARTIDA.PUBLICA
   ) {
     const parsedDate = new Date(data_partida);
     if (
@@ -35,6 +36,7 @@ export class CreatePartidaQuery {
       jogadoresIds,
       status: STATUS_PARTIDA.AGENDADA,
       num_max_jogadores,
+      tipo,
       createdAt: new Date(),
     };
     partidas.push(newPartida);
