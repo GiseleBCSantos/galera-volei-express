14864e0296dc112f00a1fb3e4f5d1d22c8f48afc
GiseleBCSantos <giselebianca57@gmail.com>
2025-10-12 13:20:01 -0300

    feat: add jogadores and partidas routes to the main router

--- src/presentation/routes/index.ts
+++ src/presentation/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { arena_router } from "./rotas_arenas";
+import { jogadores_router } from "./rotas_jogadores";
+import { partidas_router } from "./rotas_partidas";
 
 const router = Router();
 
 router.use("/arenas", arena_router);
+router.use("/jogadores", jogadores_router);
+router.use("/partidas", partidas_router);
 
 export { router as routes };
