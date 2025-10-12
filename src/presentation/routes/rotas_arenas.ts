a3c59e595ad3ca46f11e4846d6262101ec00d655
GiseleBCSantos <giselebianca57@gmail.com>
2025-10-12 17:09:57 -0300

    feat: Remove commented-out routes from arena_router for cleaner code

--- src/presentation/routes/rotas_arenas.ts
+++ src/presentation/routes/rotas_arenas.ts
@@ -5,8 +5,6 @@ export const arena_router = Router();
 const arena_controller = new ArenasController();
 
 arena_router.get("/", arena_controller.getAll);
-// arena_router.get("/two", arena_controller.getAll2);
-// arena_router.get("/three", arena_controller.getAll3);
 arena_router.get("/:id", arena_controller.findById);
 arena_router.post("/", arena_controller.createOne);
 arena_router.put("/:id", arena_controller.updateOne);
